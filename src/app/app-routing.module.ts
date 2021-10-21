cac6c1088da114e02c21fecba2cd0d0db6a59f48
David Winniford <dwinniford@gmail.com>
2021-10-21 17:09:22 -0500

    index redirects to drum-pad

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,8 +7,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 
 
 const appRoutes: Routes = [
-  {path: '', component: DrumsetComponent },
   {path: 'edit', component: DrumBuilderComponent},
+  {path: 'drum-pad', component: DrumsetComponent},
+  {path: '', redirectTo: '/drum-pad', pathMatch: 'full' },
   {path: '**', component: PageNotFoundComponent}
 ]
 
