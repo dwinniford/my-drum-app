bc3a9fcea5e517b9ff11fe7c0ac73f8aad68ac7a
David Winniford <dwinniford@gmail.com>
2021-10-22 10:06:08 -0500

    delete old form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { DrumsetComponent } from './drumset/drumset.component';
 import { DrumBlockComponent } from './drum-block/drum-block.component';
 import { EditDrumsetComponent } from './edit-drumset/edit-drumset.component';
-import { DrumFormComponent } from './drum-form/drum-form.component';
 import { ReactiveDrumFormComponent } from './reactive-drum-form/reactive-drum-form.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { AppRoutingModule } from './app-routing.module';
@@ -17,7 +16,6 @@ import { AppRoutingModule } from './app-routing.module';
     DrumsetComponent,
     DrumBlockComponent,
     EditDrumsetComponent,
-    DrumFormComponent,
     ReactiveDrumFormComponent,
     PageNotFoundComponent
   ],
