404bff0be8557e6b02ce9ab27659d1bdc973d62b
David Winniford <dwinniford@gmail.com>
2021-10-26 14:15:51 -0500

    add edit page styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import {ReactiveFormsModule, FormsModule} from '@angular/forms'
 
@@ -27,7 +28,8 @@ import { NavComponent } from './nav/nav.component';
     ReactiveFormsModule,
     FormsModule,
     DrumsetsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
