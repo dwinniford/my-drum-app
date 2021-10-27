f74a0f1efeccedccb7a9cb09911ea3d51efeb866
David Winniford <dwinniford@gmail.com>
2021-10-27 10:34:00 -0500

    animates drumset to edit route change

---
+++ src/app/animations.ts
@@ -0,0 +1,31 @@
+import { trigger, animateChild, group, transition, animate, style, query } from '@angular/animations'
+
+export const fadeInAnimation = trigger('routeAnimation', [
+    transition('drumset <=> edit-drumset', [
+        style({ position: 'relative' }),
+        query(':enter, :leave', [
+            style({
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%'
+            })
+        ]),
+        query(':enter', [
+            style({ opacity: 0, display: 'block' })
+        ]),
+        query(':leave', [
+            style({opacity: 1, display: 'block' })
+        ]),
+        query(':leave', animateChild()),
+        group([
+            query(':leave', [
+                animate('500ms ease-in-out', style({opacity: 0}))
+            ]),
+            query(':enter', [
+                animate('500ms 510ms ease-in-out', style({opacity: 1}))
+            ])
+        ]),
+        query(':enter', animateChild())
+    ])
+])
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { fadeInAnimation } from './animations'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [fadeInAnimation]
 })
 export class AppComponent {
   title = 'my-drum-app';
+  getAnimationData(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation
+  }
 }
